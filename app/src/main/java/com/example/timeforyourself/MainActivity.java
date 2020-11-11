a05b1616f8e1079086792c13903e33d988abac36
Edgars <edgarsdeveloper@gmail.com>
2020-11-11 15:14:04 +0200

    Added new picture and made btn2 on click to open new activity_paradise.xml

--- app/src/main/java/com/example/timeforyourself/MainActivity.java
+++ app/src/main/java/com/example/timeforyourself/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.timeforyourself;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.btn2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, Paradise .class);
+        startActivity(intent);
     }
 }
