38bf4a324a603391cf5db5321e907d5f5fdf0b49
Edgars <edgarsdeveloper@gmail.com>
2020-11-13 02:12:05 +0200

    Changed the backgrounds

--- app/src/main/java/com/example/timeforyourself/FreeYourMind.java
+++ app/src/main/java/com/example/timeforyourself/FreeYourMind.java
@@ -9,6 +9,6 @@ public class FreeYourMind extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_free_your_mind);
+        setContentView(R.layout.activity_free_your);
     }
 }
